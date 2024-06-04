90c67147791fbd3de01a0f7cff154e889e7de777
celia_rjs <celia.rojas@outlook.fr>
2024-06-04 15:28:41 +0200

    correction import + ajout home dans app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './features/home/home/home.component';
+import { HomeComponent } from './features/home/home.component';
 import { SignInComponent } from './features/sign-in/sign-in.component';
 import { ProductGridComponent } from './features/product-grid/product-grid.component';
 
