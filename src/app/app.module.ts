586e9c944bdbaec2ae6a2808b8b26ef972198e47
LemonBlanc <83752129+BWBMaxime@users.noreply.github.com>
2024-06-04 15:22:48 +0200

    Ajout component et service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './features/home/home/home.component';
+import { SignInComponent } from './features/sign-in/sign-in.component';
+import { ProductGridComponent } from './features/product-grid/product-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    ProductGridComponent
   ],
   imports: [
     BrowserModule,
