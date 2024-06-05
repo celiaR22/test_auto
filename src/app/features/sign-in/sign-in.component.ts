aa1274d0493541be4cda0047fca32e96356c0058
celia_rjs <celia.rojas@outlook.fr>
2024-06-05 11:40:34 +0200

    gestion des erreurs sur le formulaire

--- src/app/features/sign-in/sign-in.component.ts
+++ src/app/features/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -28,12 +29,24 @@ export class SignInComponent implements OnInit {
       firstname: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, this.customPasswordValidator()]],
       confirmPassword: ['', [Validators.required]],
-      postalCode: ['']
+      postalCode: ['', [this.frenchPostalCodeValidator()]]
     }, { validators: this.passwordMatchValidator });
   }
 
   validateForm() {
-    console.log('test');
+    if (this.signInForm.valid) {
+      const formData = this.getFormData();
+      console.log(formData);
+    }
+  }
+
+  getFormData() {
+    return {
+      lastname: this.signInForm.get('lastname')?.value,
+      firstname: this.signInForm.get('firstname')?.value,
+      password: this.signInForm.get('password')?.value,
+      postalCode: this.signInForm.get('postalCode')?.value
+    };
   }
 
   customPasswordValidator(): ValidatorFn {
@@ -54,11 +67,47 @@ export class SignInComponent implements OnInit {
   }
 
   passwordMatchValidator: ValidatorFn = (formGroup: AbstractControl): ValidationErrors | null => {
-
     const password = formGroup.get('password')?.value;
     const confirmPassword = formGroup.get('confirmPassword')?.value;
-    console.log(password === confirmPassword ? null : { passwordsMismatch: true });
-
     return password === confirmPassword ? null : { passwordsMismatch: true };
   }
+
+  frenchPostalCodeValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const postalCodeRegex = /^(?:[0-8]\d|9[0-8])\d{3}$/;
+      const isValid = postalCodeRegex.test(control.value);
+
+      return isValid ? null : { invalidPostalCode: true };
+    };
+  }
+
+  getFormFieldErrors(fieldName: string): string[] {
+    const errors: string[] = [];
+    const control = this.signInForm.get(fieldName);
+
+    if (!control || !control.touched) {
+      return errors;
+    }
+
+    if (control.errors) {
+      if (control.errors['required']) {
+        errors.push(`Le champ ${fieldName} est requis.`);
+      }
+
+      if (fieldName === 'confirmPassword' && this.signInForm.hasError('passwordsMismatch')) {
+        errors.push('Les mots de passe ne correspondent pas.');
+      }
+
+      if (fieldName === 'password' && control.errors['passwordStrength']) {
+        errors.push('Le mot de passe doit avoir au moins 8 caractères, une majuscule et un caractère spécial.');
+      }
+
+      if (fieldName === 'postalCode' && control.errors['invalidPostalCode']) {
+        errors.push('Le code postal n\'est pas valide.');
+      }
+    }
+
+    return errors;
+  }
+
 }
